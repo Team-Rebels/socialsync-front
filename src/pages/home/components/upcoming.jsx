fe16994ef8924347a4b96ac926ecc777c81a0066
CharlesAduSark <charlesadusarkodie@gmail.com>
2024-07-04 13:39:18 +0000

    Updates to upcoming

--- src/pages/home/components/upcoming.jsx
+++ src/pages/home/components/upcoming.jsx
@@ -4,7 +4,7 @@ import { tour3 } from '../../../assets/images'
 const Upcoming = () => {
   return (
     <div className='h-[90vh]'> 
-        <div className='flex flex-row mt-[70px] pb-[30px]'>
+        <div className='flex flex-row mt-[70px] pb-[30px] pl-[30px]'>
             <h1>Highlights for</h1>
             <select className="p-2 rounded-lg">
                 <option value="Select Date" disabled selected hidden>Select Date</option>
@@ -14,7 +14,7 @@ const Upcoming = () => {
             <div className='w-[750px] w-1/2'>
                 <img src={tour3} alt="" />
             </div>
-            <div className='w-[750px] w-1/2'>
+            <div className='w-[750px] w-1/2 bg-[#6AA2EF]'>
                 <p>Hello</p>
                 <p>Hi</p>
             </div>
